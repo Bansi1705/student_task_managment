7b92e7b9f004b87423a067eb68280a8759d98697
Bansi Chudasama <bansi1705@gmail.com>
2026-01-30 13:50:16 +0530

    [BC][Register component created]

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from './components/Header'
+import Register from './pages/Register'
 
 function App() {
 
   return (
     <>
      <Header/>
+     <Register/>
     </>
   )
 }
---
+++ src/pages/Register.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Register() {
+  return (  
+    <>
+    <div className='form-container'> 
+      <h1>Register</h1>
+      <form action="">
+        <input type="text" placeholder='Name' /><br />
+        <input type="text" placeholder='email' /><br />
+        <input type="password" placeholder='password' /><br />
+      </form>
+      </div>
+    </>
+  )
+}
+
+export default Register
