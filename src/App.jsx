da0619d5f2cced4c359ba8346496799c9f87c4e1
Bansi Chudasama <bansi1705@gmail.com>
2026-02-02 13:31:32 +0530

    [BC][Default Router added]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 import './App.css'
 // import Header from './components/Header'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
+const DefaultRoute=()=>{
+  const authData=JSON.parse(localStorage.getItem("authData"));
+  if(authData){
+    return <Navigate to="/login" replace/>
+  }
+  return <Navigate to="/register" replace/>
+}
+
 function App() {
 
  const route=createBrowserRouter([
+  {
+    path:"/",
+    element:<DefaultRoute/>
+  },
   {
     path:"/login",
     element:<Login/>
