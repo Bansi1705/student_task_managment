3fb1a6bfa7d7027a12ab3297a3908dcdd09ff6d8
Bansi Chudasama <bansi1705@gmail.com>
2026-02-03 12:07:49 +0530

    [BC][auth gaurd created]

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,56 @@
-import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
-import './App.css'
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
+import "./App.css";
 // import Header from './components/Header'
-import Login from './pages/Login'
-import Register from './pages/Register'
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Dashboard from "./pages/Dashboard";
+import AuthGuard from "./auth/AuthGuard";
 
-const DefaultRoute=()=>{
-  const authData=JSON.parse(localStorage.getItem("authData"));
-  if(authData){
-    return <Navigate to="/login" replace/>
+const DefaultRoute = () => {
+  const lData = JSON.parse(localStorage.getItem("lData"));
+  if (lData) {
+    return <Navigate to="/dashboard" replace />;
   }
-  return <Navigate to="/register" replace/>
-}
+  return <Navigate to="/login" replace />;
+};
 
 function App() {
+  const route = createBrowserRouter([
+    {
+      path: "/",
+      element: <DefaultRoute />,
+    },
+    {
+      path: "/login",
+      element: (
+        <AuthGuard required={false}>
+          <Login />
+        </AuthGuard>
+      ),
+    },
+    {
+      path: "/register",
+      element:(
+        <AuthGuard required={false}>
+          <Register />
+        </AuthGuard>
+      ),
+    },
+    {
+      path: "/dashboard",
+      element: (
+        <AuthGuard required={true}>
+          <Dashboard />
+        </AuthGuard>
+      ),
+    },
+  ]);
 
- const route=createBrowserRouter([
-  {
-    path:"/",
-    element:<DefaultRoute/>
-  },
-  {
-    path:"/login",
-    element:<Login/>
-  },
-  {
-    path:"/register",
-    element:<Register/>
-  },
- ])
-
- return <RouterProvider router={route}/>
+  return <RouterProvider router={route} />;
 }
 
-export default App
+export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,12 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 
 function Login() {
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const [error, setError] = useState({});
+
+  const navigate=useNavigate();
+
+  const handleChange = (e) => {
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+    setError({
+      ...error,
+      [e.target.name]: "",
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (validate()) {
+      const saveData = JSON.parse(localStorage.getItem("authData"));
+      if (
+        saveData.email === data.email &&
+        saveData.password === data.password
+      ) {
+        localStorage.setItem("lData",JSON.stringify(data))
+        alert("Login Succesfully ......");
+        navigate("/dashboard");
+      }
+    } else {
+      alert("Somthing went Wrong");
+    }
+  };
+  const validate = () => {
+    const newError = {};
+    if (!data.email.trim()) {
+      newError.email = "Email is Required";
+    } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(data.email)) {
+      newError.email = "Invalide Email formate.";
+    }
+    if (!data.password.trim()) {
+      newError.password = "Password is Required.";
+    } else if (data.password.length < 6) {
+      newError.password = "Minimum 6 Character Required.";
+    }
+    setError(newError);
+    return Object.keys(newError).length === 0;
+  };
   return (
     <div className="form-container">
       <h1 className="form-title">Welcome Back</h1>
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         {/* email feild */}
         <div className="form-group">
           <label htmlFor="email">Email Address</label>
@@ -15,6 +66,8 @@ function Login() {
             placeholder="Enter your email"
             name="email"
             id="email"
+            value={data.email}
+            onChange={handleChange}
           />
         </div>
         {/* password feild */}
@@ -25,6 +78,8 @@ function Login() {
             placeholder="Create a password"
             name="password"
             id="password"
+            value={data.password}
+            onChange={handleChange}
           />
         </div>
         {/* submit button */}
@@ -33,8 +88,8 @@ function Login() {
         </button>
       </form>
       <p className="link-text">
-          Don't have an account? <Link to="/register">Register Here</Link>
-        </p>
+        Don't have an account? <Link to="/register">Register Here</Link>
+      </p>
     </div>
   );
 }
